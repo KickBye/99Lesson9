2d4b8021cb7659d624677a3955f5fc066970f88a
KickBye <102456590+KickBye@users.noreply.github.com>
2022-05-01 23:00:34 +0300

    Update DBGetTest.java

--- src/test/java/dbTests/DBGetTest.java
+++ src/test/java/dbTests/DBGetTest.java
@@ -1,26 +1,3 @@
-
-////
-////import db.DBHelper;
-////import org.testng.annotations.Test;
-////
-////public class DBGetTestDZ7 {
-////
-////    @Test
-////    public void startTest() {
-////        String sql = "SELECT * FROM REGISTRATION";
-////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
-////        System.out.println(DBHelper.getValueFirstRow(sql, "first"));
-////
-////        String sql2 = "INSERT REGISTRATION(id, last, first, age) VALUES (20, 'lastname6', 'name6', 31)";
-////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
-////        System.out.println("Добавлено строк: " + DBHelper.getInsertSQL(sql2));
-////
-////        String sql3 = "INSERT REGISTRATION(id, last, first, age) VALUES (21, 'lastname7', 'name7', 19)";
-////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
-////        System.out.println("Добавлено строк: " + DBHelper.getInsertSQL(sql2) + DBHelper.getInsertSQL(sql3));
-////
-////    }
-////}
 package dbTests;
 
 import db.DBHelper;
